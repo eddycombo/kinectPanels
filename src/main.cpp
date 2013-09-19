81b6e6d427813e7cbbcc25988eebf3e68f716527
eddycombo <eddy@combosquare.net>
2013-09-19 15:28:16 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "testApp.h"
+
+int main() {
+    ofSetupOpenGL(1280, 532, OF_WINDOW);
+	ofRunApp(new testApp());
+}
---
+++ src/testApp.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+
+// uncomment this to read from two kinects simultaneously
+//#define USE_TWO_KINECTS
+
+class testApp : public ofBaseApp {
+public:
+
+	void setup();
+	void update();
+	void draw();
+	void exit();
+
+	void drawPointCloud();
+
+	void keyPressed(int key);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+
+	ofxKinect kinect;
+  ofxCvGrayscaleImage grayImage, bgImage, diffImage; // grayscale depth image
+
+
+  ofImage savedFrontGradient, savedBackGradient;
+
+  int nearThreshold, farThreshold, nearThreshold2, farThreshold2;
+  int panelsDiff;
+	int kinectAngle;
+  bool saveGradients, checkAngle;
+  int activePointIn;
+	int nearTY, farTY, nearT2Y, farT2Y;
+
+  ofPoint  panelsCtls[4]; //[near, far, near2, far2]
+  ofPoint refPoint;
+
+  float pan1angle, pan2angle;
+
+  ofVec2f refVec, nearVec, farVec;
+  float corrAngle, kAngle;
+
+	//different method using 2 gradients.
+	ofxCvGrayscaleImage frontPanelGradient;
+  ofxCvGrayscaleImage backPanelGradient;
+
+  void computePanels(ofxCvGrayscaleImage & gray, ofxCvGrayscaleImage & frontGradient, ofxCvGrayscaleImage & backGradient, int & near1, int & near2, int & far1, int & far2);
+  void isPixelBetweenPanels(ofxCvGrayscaleImage & gray, ofxCvGrayscaleImage & frontGradient, ofxCvGrayscaleImage & backGradient);
+  void drawGoodPoints(ofxCvGrayscaleImage & gray);
+
+
+};
